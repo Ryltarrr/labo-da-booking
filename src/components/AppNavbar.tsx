afb914ac9668228f867af79855862dc12455918c
Justin Guerré <guerrejustin@gmail.com>
2022-11-15 22:33:08 +0100

    add layout and nice navbar

---
+++ src/components/AppNavbar.tsx
@@ -0,0 +1,139 @@
+import {
+  ActionIcon,
+  Box,
+  Divider,
+  Flex,
+  Navbar,
+  NavLink,
+  Text,
+} from "@mantine/core";
+import {
+  IconCalendarPlus,
+  IconCertificate,
+  IconCheck,
+  IconHome,
+  IconLogin,
+  IconLogout,
+  IconMail,
+  IconSchool,
+} from "@tabler/icons";
+import { signOut, signIn, useSession } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+type AppNavbarProps = {
+  opened: boolean;
+};
+
+type AppLink = {
+  label: string;
+  href: string;
+  icon: JSX.Element;
+  visibility: "STUDENT" | "TEACHER" | "ALL";
+};
+
+const links: AppLink[] = [
+  {
+    label: "Accueil",
+    href: "/",
+    icon: <IconHome size={22} stroke={1.5} />,
+    visibility: "ALL",
+  },
+  {
+    label: "Formatteurs",
+    href: "/teachers",
+    icon: <IconSchool size={22} stroke={1.5} />,
+    visibility: "TEACHER",
+  },
+  {
+    label: "Réservation",
+    href: "/bookings",
+    icon: <IconCalendarPlus size={22} stroke={1.5} />,
+    visibility: "STUDENT",
+  },
+  {
+    label: "Validation",
+    href: "/bookings/validate",
+    icon: <IconCheck size={22} stroke={1.5} />,
+    visibility: "TEACHER",
+  },
+  {
+    label: "Invitation",
+    href: "/teachers/invite",
+    icon: <IconMail size={22} stroke={1.5} />,
+    visibility: "TEACHER",
+  },
+  {
+    label: "Formation",
+    href: "/teachers/courses",
+    icon: <IconCertificate size={22} stroke={1.5} />,
+    visibility: "TEACHER",
+  },
+];
+
+const AppNavbar: React.FC<AppNavbarProps> = ({ opened }) => {
+  const { data: sessionData } = useSession();
+  const router = useRouter();
+
+  const filteredLinks = links.filter((link) => {
+    if (link.visibility === "TEACHER" && !sessionData) {
+      return false;
+    } else if (link.visibility === "STUDENT" && sessionData) {
+      return false;
+    }
+    return true;
+  });
+
+  return (
+    <Navbar
+      p="md"
+      hiddenBreakpoint="sm"
+      hidden={!opened}
+      width={{ sm: 200, lg: 300 }}
+    >
+      <Navbar.Section grow mt="md">
+        {filteredLinks.map((link) => (
+          <Link key={link.href} href={link.href} passHref legacyBehavior>
+            <NavLink
+              active={router.route === link.href}
+              label={link.label}
+              component="a"
+              icon={link.icon}
+            />
+          </Link>
+        ))}
+      </Navbar.Section>
+      <Divider my="sm" />
+      <Navbar.Section>
+        <Flex justify="space-around" align="center">
+          {sessionData ? (
+            <>
+              <Box>
+                <Text fw={500}>{sessionData.user?.name}</Text>
+                <Text c="dimmed">{sessionData.user?.email}</Text>
+              </Box>
+              <ActionIcon
+                onClick={() => {
+                  signOut();
+                  router.replace("/");
+                }}
+              >
+                <IconLogout />
+              </ActionIcon>
+            </>
+          ) : (
+            <>
+              <Text>Connexion formatteur</Text>
+
+              <ActionIcon onClick={() => signIn()}>
+                <IconLogin />
+              </ActionIcon>
+            </>
+          )}
+        </Flex>
+      </Navbar.Section>
+    </Navbar>
+  );
+};
+
+export default AppNavbar;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,56 @@
-import { useSession } from "next-auth/react";
-import type { ReactNode } from "react";
+import {
+  AppShell,
+  Burger,
+  Center,
+  Footer,
+  Header,
+  MediaQuery,
+  useMantineTheme,
+} from "@mantine/core";
+import Image from "next/image";
+import { type ReactNode, useState } from "react";
+import logo from "../../public/logo.png";
+import AppNavbar from "./AppNavbar";
 
 function Layout({ children }: { children: ReactNode }) {
-  const session = useSession();
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
   return (
-    <>
-      <nav>Bonjour {session.data?.user?.name}</nav>
+    <AppShell
+      styles={{
+        main: {
+          background: theme.colors.gray[0],
+        },
+      }}
+      navbarOffsetBreakpoint="sm"
+      navbar={<AppNavbar opened={opened} />}
+      footer={
+        <Footer height={60} p="md">
+          <Center>&copy; Labo-DA 2022</Center>
+        </Footer>
+      }
+      header={
+        <Header height={{ base: 50, md: 70 }} p="md">
+          <div
+            style={{ display: "flex", alignItems: "center", height: "100%" }}
+          >
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+
+            <Image src={logo} width={100} alt="Logo Labo-DA"></Image>
+          </div>
+        </Header>
+      }
+    >
       <main>{children}</main>
-    </>
+    </AppShell>
   );
 }
 
